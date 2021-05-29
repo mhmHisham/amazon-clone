dcdbab3612ef12de689a0adfb1e347d6f46942b3
mhmHisham <hishamhaneefa96@gmail.com>
2021-05-29 22:54:20 +0530

    user authentication with firebase

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Home from './Home';
 import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
-function App() {
+function App() {  
+  const [{} , dispatch] =useStateValue();
+
+  useEffect( () =>{
+
+    auth.onAuthStateChanged(authUser =>{
+      console.log('THE USER IS >>>' ,authUser);
+
+      if(authUser){
+        //the use is logged in / the use was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else{
+        //the user logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+    
+  },[])
+
   return (
     <Router>
       <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAzA3Hl9LFOSGr18DtnDJfDXEayz0OJ3FA",
     authDomain: "clone-eec7a.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:391997966679:web:36cc5eca89a796c688d612",
     measurementId: "G-M94C5VM2T4"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db , auth};
+
